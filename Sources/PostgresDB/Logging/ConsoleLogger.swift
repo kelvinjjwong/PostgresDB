725d2a8f87335ae088402c1665c0e37ad9cc079c
Kelvin Wong <kelvinjjwong@outlook.com>
2023-01-15 03:57:38 +0800

    initial

---
+++ Sources/PostgresDB/Logging/ConsoleLogger.swift
@@ -0,0 +1,63 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kelvin JJ Wong on 2023/1/15.
+//
+
+import Foundation
+
+class ConsoleLogger {
+    
+    private var category:String = ""
+    private var subCategory:String = ""
+    
+    init(category:String) {
+        self.category = category
+    }
+    
+    convenience init(category:String, subCategory:String){
+        self.init(category: category)
+        self.subCategory = subCategory
+    }
+    
+    private func prefix() -> String {
+        if subCategory == "" {
+            return "\(Date()) [\(category)]"
+        }else{
+            return "\(Date()) [\(category)][\(subCategory)]"
+        }
+    }
+    
+    public func timecost(_ message:String, fromDate:Date){
+        log("\(message) - time cost: \(Date().timeIntervalSince(fromDate)) seconds")
+    }
+    
+    public func log(_ message:String){
+        print("\(prefix()) \(message)")
+    }
+    
+    public func log(_ message:Int){
+        print("\(prefix()) \(message)")
+    }
+    
+    public func log(_ message:Double){
+        print("\(prefix()) \(message)")
+    }
+    
+    public func log(_ message:Float){
+        print("\(prefix()) \(message)")
+    }
+    
+    public func log(_ message:Any){
+        print("\(prefix()) \(message)")
+    }
+    
+    public func log(_ error:Error){
+        print("\(prefix()) \(error)")
+    }
+    
+    public func log(_ message:String, _ error:Error){
+        print("\(prefix()) \(message) - \(error)")
+    }
+}
--- Sources/PostgresDB/PostgresDB.swift
+++
@@ -1,6 +0,0 @@
-public struct PostgresDB {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
